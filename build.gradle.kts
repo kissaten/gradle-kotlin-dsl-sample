dcac8390f8184205c399a81638dcfd3dc559e1c9
Joe Kutner <jpkutner@gmail.com>
2017-06-29 11:21:59 -0500

    first

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    kotlin("jvm")
+    application
+}
+
+application {
+    mainClassName = "samples.HelloWorldKt"
+}
+
+dependencies {
+    compile(kotlin("stdlib"))
+    compile("org.eclipse.jetty:jetty-servlet:9.4.6.v20170531")
+    compile("javax.servlet:javax.servlet-api:3.1.0")
+}
+
+repositories {
+    jcenter()
+}
